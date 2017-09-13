405c1b7ea887ca7dcdb296052438380dd7a5959d
Patrick Faust <lipaonline@gmail.com>
2017-09-13 11:30:24 +0200

    ajout d'un filtre

--- wp_head.php
+++ wp_head.php
@@ -11,7 +11,10 @@ License: GPLv2+
 add_action('wp_head', 'hook_wp_head');
 
 function hook_wp_head() {
-    ?>
+    
+    ob_start();
+
+ ?>
         <!-- ici le code à ajouter dans Head
         Par exemple un code Google Analytics, un code de vérification Pinterest, régie de pub...
         Note: pour ajouter un script js, il vaut mieux faire autrement.. que l'on verra plus tard...
@@ -19,4 +22,11 @@ function hook_wp_head() {
 
         -->
     <?php
+
+$my_head = apply_filters('lipa_my_head', ob_get_clean());
+
+echo $my_head;
+    
+    
+   
 }
